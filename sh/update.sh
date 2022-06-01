dd46ee085c6c633b841d05dfc2833df5f6e77368
johnny <934004841@qq.com>
2022-06-01 20:18:08 +0800

    chore: remove rc dir

--- sh/update.sh
+++ sh/update.sh
@@ -1,10 +1,6 @@
 #!/bin/zsh
 
-# copy 指定路径
-cp src/rc/.zshrc ~/.zshrc
-cp src/.sh/do-gl.sh ~/.sh/do-gl.sh
-cp src/.sh/ghc.sh ~/.sh/ghc.sh
-cp src/.sh/tmux-work.sh ~/.sh/tmux-work.sh
+cp -r src/. ~/
 
 source ~/.zshrc
 echo "update done"
