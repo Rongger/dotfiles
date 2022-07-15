c48de0fdbaf937e84e9233b8add920339dbc6aa8
johnny <934004841@qq.com>
2022-07-15 14:29:46 +0800

    chore: install plugins

--- install.sh
+++ install.sh
@@ -31,8 +31,21 @@ shopt -s expand_aliases
 proxy_on
 
 zsh_theme=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-if [ -d "$zsh_theme" ]; then
+if [ ! -d "$zsh_theme" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $zsh_theme
+  echo "✅ powerlevel10k"
+fi
+
+zsh_syntax_highlighting=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d "$zsh_syntax_highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_syntax_highlighting
+  echo "✅ zsh-syntax-highlighting"
+fi
+
+zsh_autosuggestions=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d "$zsh_autosuggestions" ]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions $zsh_autosuggestions
+  echo "✅ zsh-autosuggestions"
 fi
 
 echo "👌 Install done"
