f7399952a2d410b56e8b0a13eec2b1e22c55f7d5
johnny <934004841@qq.com>
2022-08-15 21:09:29 +0800

    chore: vscode go extension deps

--- install/golang.sh
+++ install/golang.sh
@@ -11,6 +11,11 @@ if [ ! -d /usr/local/go ]; then
 
   export PATH=$PATH:/usr/local/go/bin
 
+  # for VS Code Go extension 
+  go install -v github.com/ramya-rao-a/go-outline@latest
+  go install -v golang.org/x/tools/gopls@latest
+  go install -v golang.org/x/tools/cmd/goimports@latest
+
   echo '👌 Golang install done'
 else
   export PATH=$PATH:/usr/local/go/bin
