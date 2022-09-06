31a73abcd6211ae7fb17b9f294a5be800bf7615d
johnny <934004841@qq.com>
2022-09-06 18:01:22 +0800

    chore: update

---
+++ sh/lint.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# node@18
+yarn add -D husky lint-staged prettier
+
+npx husky add .husky/pre-commit 'npx lint-staged'
+
+npm pkg set scripts.precommit="lint-staged"
+
+npm pkg set husky.hooks.pre-commit="npm run precommit"
+
+npm pkg set lint-staged['src/*'][0]="eslint --fix"
+
+# 手动添加
+echo 'package.json 手动添加: \n"lint-staged": {
+  "src/**/*.{js?(x),ts?(x)}": [
+    "eslint --fix",
+    "prettier --write"
+  ],
+  "app/**/*.{less,css,md,json}": [
+    "prettier --write"
+  ]
+}'
